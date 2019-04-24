76022834c64f11a92356b23c5298991ec79054a4
Arilox <arilox9@gmail.com>
2019-04-24 14:06:35 +0200

    Getting PRODuctive

--- back/app.js
+++ back/app.js
@@ -1,9 +1,13 @@
+const https = require('https');
 const express = require("express");
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 const app = express();
 const router = require('./router');
+const fs = require('fs');
+const helmet = require("helmet");
+const pswd = require("./config.js");
 
 app.use(cors()); //https://expressjs.com/en/resources/middleware/cors.html
 app.use(express.json());
@@ -14,7 +18,13 @@ app.use(bodyParser.json());
 
 app.use('/', router);
 
-var port = 3001;
-app.listen(port, () => {
+app.use(helmet());
+
+var port = 443;
+https.createServer({
+	key: fs.readFileSync('key.pem'),
+	cert: fs.readFileSync('cert.pem'),
+	passphrase: pswd
+}, app).listen(port, () => {
     console.log(`Listening on port ${port}`)
 });
